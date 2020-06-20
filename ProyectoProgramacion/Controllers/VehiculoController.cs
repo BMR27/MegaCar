b9c7f9970841e06a801970f4d3badb4a4f1e19c1
Andrey Azofeifa Alvarado <andrey_visual18@outlook.com>
2020-06-19 22:20:33 -0600

    Formulario de Veiculo

---
+++ ProyectoProgramacion/Controllers/VehiculoController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoProgramacion.Controllers
+{
+    public class VehiculoController : Controller
+    {
+        // GET: Vehiculo
+        public ActionResult RegistraVehiculo()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        //public ActionResult RegistraVehiculo()
+        //{
+        //    return View();
+        //}
+    }
+}
---
+++ ProyectoProgramacion/Scripts/RegistroVehiculo/RegistroVehiculo.js
@@ -0,0 +1,54 @@
+﻿$(function () {
+    ValidarRegistroVehiculo();
+    ToolTip();
+});
+/*Validamos los campos del formulario*/
+function ValidarRegistroVehiculo() {
+    $("#frmRegistraVehiculo").validate(
+        {
+            rules: {
+                C_PLACA: {
+                    required: true,
+                    maxlength: 6,
+                    minlength: 6
+                },
+                C_CANT_PUERTAS: {
+                    required: true,
+                    digits: true,
+                    maxlength: 1,
+                    minlength: 1
+                },
+                C_CANT_RUEDAS: {
+                    required: true,
+                    digits: true,
+                    maxlength: 1,
+                    minlength: 1
+                },
+                C_YEAR: {
+                    required: true,
+                    digits: true,
+                    maxlength: 4,
+                    minlength:4
+                },
+                IdModelo: {
+                    required: true
+                },
+                IdTipo: {
+                    required: true
+                },
+                IdMarca: {
+                    required: true
+                }
+
+            }
+
+        }
+    );
+};
+
+/*Agregamos un tooltip*/
+function ToolTip() {
+    $("#C_PLACA").tooltip({
+        trigger: "click"
+    });
+};
