279e7c34bb9f6f1118ed763fd052b94c2aebe5b2
Bryan Mejia Ruiz <bmr1919@hotmail.com>
2020-06-24 13:34:50 -0600

    Creacion de Cliente

---
+++ ProyectoProgramacion/Scripts/Registro Cliente/RegistroCliente.js
@@ -0,0 +1,49 @@
+﻿
+
+$(function () {
+    creaValidaciones();
+});
+
+///crea las validaciones para el formulario
+function creaValidaciones() {
+    $("#frmRegistraCliente").validate({
+        ///objeto que contiene "las condiciones" que el formulario
+        ///debe cumplir para ser considerado válido
+        rules: {
+            C_NOMBRE_CLIENTE: {
+                required: true,
+                minlength: 3,
+                maxlength: 7
+            },
+            C_APELLIDO1: {
+                required: true
+            },
+            C_APELLIDO2: {
+                required: true
+            },
+            C_CEDULA: {
+                required: true,
+                email: true
+            },
+            C_CORREO: {
+                required: true,
+                maxlength: 12
+            },
+            C_DIRECCION: {
+                required: true
+            },
+            C_TELEFONO: {
+                required: true
+            },
+            id_Provincia: {
+                required: true
+            },
+            id_Canton: {
+                required: true
+            },
+            id_Distrito: {
+                required: true
+            }
+        }
+    });
+}
