1693d4326c81bfdabe8e84a3faf5f3842d335068
Bryan Mejia Ruiz <bmr1919@hotmail.com>
2020-09-01 00:12:08 -0600

    prueba

--- ProyectoProgramacion/Controllers/Reporte_ServicioCliente_Controller.cs
+++ ProyectoProgramacion/Controllers/Reporte_ServicioCliente_Controller.cs
@@ -3,15 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ProyectoProgramacion.Modelo;
 
 namespace ProyectoProgramacion.Controllers
 {
     public class Reporte_ServicioCliente_Controller : Controller
     {
+
+        #region INSTANCIAS
+        programacionBDEntities ModeloDB = new programacionBDEntities();
+        #endregion
         // GET: Reporte_ServicioCliente_
-        public ActionResult Index()
+        public ActionResult Reportes_ServicioCliente()
         {
             return View();
+        }
+
+        //CONSULTA LOS SERVICIOS
+        [HttpPost]
+        public ActionResult MostrarServicios()
+        {
+            List<SP_REPORTE_SERVICIO_CLIENTE_Result> ListaServicios =
+                this.ModeloDB.SP_REPORTE_VEHICULO_CLIENTE(null).ToList();
+
+            return Json(new
+            {
+                resultado = ListaServicios
+            });
         }
     }
 }
