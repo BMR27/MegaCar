0b60ba20d927991a3a28afad75ccbf99ccafd4ce
Bryan Mejia Ruiz <bmr1919@hotmail.com>
2020-08-31 22:39:28 -0600

    sp 2

---
+++ ProyectoProgramacion/Controllers/Reporte_ServicioCliente_Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoProgramacion.Controllers
+{
+    public class Reporte_ServicioCliente_Controller : Controller
+    {
+        // GET: Reporte_ServicioCliente_
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
