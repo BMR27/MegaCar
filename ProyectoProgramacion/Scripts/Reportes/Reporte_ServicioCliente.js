cf6631a40820fb805070cae7cf1c1b6f1c7d77b8
Andrey Azofeifa Alvarado <andrey_visual18@outlook.com>
2020-09-01 22:55:59 -0600

    servicios

--- ProyectoProgramacion/Scripts/Reportes/Reporte_ServicioCliente.js
+++ ProyectoProgramacion/Scripts/Reportes/Reporte_ServicioCliente.js
@@ -1,12 +1,39 @@
 ﻿//Document ready
 $(function () {
-    ConsultarListaServicios();
-    OcultarAlertasBotones();
-    CapturarDatosFormulario();
-    ConsultarListaCliente();
-    ObetenerDatoGrid();
+    //ConsultarListaServicios();
+    //OcultarAlertasBotones();
+    //CapturarDatosFormulario();
+    MostrarClientes();
+    //ObetenerDatoGrid();
+    EventoClick();
 });
 
+function EventoClick() {
+    $("#btnConsultar").on("click", function () {
+        ConsultarListaServicios();
+    });
+}
+
+
+//FUNCION MUESTRA LOS CLIENTES
+function MostrarClientes() {
+    var urlMetodo = '/Factura/MostrarClientes'
+    var parametros = {};
+    var funcion = procesarResultadoCliente;
+    ejecutaAjax(urlMetodo, parametros, funcion);
+}
+
+function procesarResultadoCliente(data) {
+    var ddlCliente = $("#cliente");
+    ddlCliente.empty();
+    var nuevaOpción = "<option value=''>Seleccione una Cliente</option>";
+    ddlCliente.append(nuevaOpción);
+    $(data).each(function () {
+        var ClienteActual = this;
+        nuevaOpción = "<option value='" + ClienteActual.C_ID_CLIENTE + "'>" + ClienteActual.C_NOMBRE_CLIENTE + " " + ClienteActual.C_APELLIDO1 + " " + ClienteActual.C_APELLIDO2 + "</option>";
+        ddlCliente.append(nuevaOpción);
+    });
+}
 //OCULTAR ALERTAS Y BOTONES
 function OcultarAlertasBotones() {
     $("#AlertaExito").hide();
@@ -37,8 +64,10 @@ function ValidarRegistroFabricante() {
 /*consulta la lista de paises*/
 function ConsultarListaServicios() {
     /////construir la dirección del método del servidor
-    var urlMetodo = '/Reporte_ServicioCliente/MostrarServicios'
-    var parametros = {};
+    var urlMetodo = '/Reporte_ServicioCliente_/MostrarServicios'
+    var parametros = {
+        C_ID_CLIENTE: $("#cliente").val()
+    };
     var funcion = creaGrid;
     ///ejecuta la función $.ajax utilizando un método genérico
     //para no declarar toda la instrucción siempre
@@ -49,7 +78,7 @@ function creaGrid(data) {
     $("#divListaServicios").kendoGrid({
         dataSource: {
             data: data.resultado,
-            pageSize: 3
+            pageSize: 15
         },
         pageable: true,
         columnMenu: true,
@@ -58,12 +87,12 @@ function creaGrid(data) {
         toolbar: ["search"],
         columns: [
             {
-                field: 'C_ID_SERVICIO',
-                title: 'Id Servicio'
+                field: 'C_NOMBRE_SERVICIO',
+                title: 'Nombre Servicio'
             },
             {
-                field: 'C_NOMBRE_CLIENTE',
-                title: 'Nombre'
+                field: 'C_NOMBRE_MARCA',
+                title: 'Nombre Marca'
             }
             
         ]
